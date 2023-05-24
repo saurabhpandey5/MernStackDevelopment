65c22d0f3b9d1f60712bfc1eec9735de6016e8aa
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-05-24 12:32:18 +0530

    Random Quotes generator

---
+++ ReactProjects/random-quote/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Quotes from './Component/Quotes';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Quotes/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
